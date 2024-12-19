08b5de845b8198bfdb215539ec2e6ba34c0dd33c
gestefania2 <gestefania2@gmail.com>
2024-12-19 12:23:33 +0100

    definir apiCallController para el fetchData

---
+++ src/api/apiCallController.js
@@ -0,0 +1,35 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+
+async function fetchData(pathName, method = 'GET', body = null){
+    try {
+        const url = new URL(BASE_URL);
+        url.pathname = pathName;
+        console.log("Url complete:" +url.toString());
+        console.log("Body complete:" + JSON.stringify(body));
+        const token = localStorage.getItem("authToken");
+        console.log("Profile complete:"+ token);
+        const options = {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                accept: 'application/json',
+                'Authorization': `Bearer ${token? token : ""}`,
+            },
+            body:body ? JSON.stringify(body) : null,
+        };
+        const response = await fetch(url.toString(), options);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data =await response.json();
+        return data;
+    } catch (error) {   
+        console.error("Error fetching data:", error);
+        throw error;
+        
+        }
+}
+
+export default fetchData;
